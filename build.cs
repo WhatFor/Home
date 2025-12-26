3a0c315fcb68be78141e5f1615119c35b25306d8
Chris Walker <chris@awsm.tech>
2025-12-26 14:33:34 +0000

    Improved build; Added post on AI opinions

--- build.cs
+++ build.cs
@@ -7,6 +7,8 @@ const string PAGES_DIR = "./src/";
 const string PAGES_DIR_OUTPUT = "./dist/";
 const string MAIN_TEMPLATE = "./src/_templates/main.html";
 
+const string TEMPLATES_DIR = "./src/_templates/";
+
 const string ASSETS_DIR = "./src/assets/";
 const string ASSETS_DIR_OUTPUT = "./dist/assets";
 
@@ -15,6 +17,28 @@ const string BODY_PLACEHOLDER = "<!--#CONTENT#-->";
 
 // Helpers
 bool IsTemplate(string path) => Path.GetFileName(path).StartsWith('_');
+
+string ParseAndSanitisePath(string path) => Path.GetFullPath(path).Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+
+bool IsBuildPath(string path)
+{
+    var p = ParseAndSanitisePath(path);
+    var posts = ParseAndSanitisePath(POSTS_DIR);
+    var assets = ParseAndSanitisePath(ASSETS_DIR);
+    var templates = ParseAndSanitisePath(TEMPLATES_DIR);
+
+    return p.StartsWith(posts) || p.StartsWith(assets) || p.StartsWith(templates);
+}
+
+string ReplaceLeadingPathSegment(string path, string segment, string replacement)
+{
+    var dir = Path.GetDirectoryName(path)! + "\\";
+    var trimmedDir = dir.Replace(segment, replacement);
+
+    var fileName = Path.GetFileName(path);
+
+    return Path.Combine(trimmedDir, fileName);
+}
 
 string ReadTemplate(string path)
 {
@@ -55,10 +79,22 @@ foreach (var post in postsHtml)
     File.WriteAllText(outputFile, postOutput);
 }
 
-// Output pages
+// Output pages;
+// Includes both top-level html files,
+// and also any nested files (preserving the path).
+// Excludes /_templates/, /assets/, /posts/, which are handled elsewhere.
 var pagesHtml = Directory
-    .GetFiles(PAGES_DIR, "*.html")
-    .Select(f => new { Name = Path.GetFileName(f), Content = File.ReadAllText(f) })
+    .GetFiles(PAGES_DIR, "*.html", new EnumerationOptions
+    {
+        RecurseSubdirectories = true
+    })
+    .Where(f => !IsBuildPath(f))
+    .Select(f => new
+    {
+        Name = Path.GetFileName(f),
+        Content = File.ReadAllText(f),
+        OutputDir = ReplaceLeadingPathSegment(f, ".\\src\\", ".\\"),
+    })
     .ToList();
 
 foreach (var page in pagesHtml)
@@ -66,7 +102,7 @@ foreach (var page in pagesHtml)
     if (IsTemplate(page.Name))
         continue;
 
-    Console.WriteLine($"Page: {page.Name}");
+    Console.WriteLine($"Page: {page.Name}. Dir: {page.OutputDir}.");
     var pageOutput = mainTemplate.Replace(BODY_PLACEHOLDER, page.Content);
 
     // Special handling for the posts.html listing page
@@ -79,7 +115,11 @@ foreach (var page in pagesHtml)
         pageOutput = pageOutput.Replace(POSTS_PLACEHOLDER, listing);
     }
 
-    var outputFile = Path.Combine(PAGES_DIR_OUTPUT, page.Name);
+    var outputFile = Path.Combine(PAGES_DIR_OUTPUT, page.OutputDir);
+
+    // Ensure dir exists
+    Directory.CreateDirectory(Path.GetDirectoryName(outputFile)!);
+
     File.WriteAllText(outputFile, pageOutput);
 }
 
